0e37f23b899750a7f262bcfefe39e952d9b7c917
moensrc <94112988+moensrc@users.noreply.github.com>
2022-03-15 11:25:59 +0100

    Add files via upload

--- opdracht2/scripts/script.js
+++ opdracht2/scripts/script.js
@@ -1,2 +1,7 @@
-// JavaScript Document
-console.log("howdy");
+// Get HTML elements and define as variables
+var instructionText = document.querySelector("section h2");
+var bingoBall = document.querySelector("section h3");
+
+var buttonNewBall = document.querySelector("section button");
+var buttonAddCard = document.querySelector("aside button");
+
